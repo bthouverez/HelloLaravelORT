d2d47b129892ea64571f2b929e02ae9ad34279d3
bthouverez <bastien.thouverez@gmail.com>
2025-09-25 15:40:19 +0200

    Route, views, data transfer, blade

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<h1>Bienvenue dans le HOME !!</h1>
+<p>
+    {{ $greeting }} {{ $nom_utilisateur }}
+</p>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    $greeting = 'Yo ';
+    $username = 'Jean-Pierre';
+    return view('home', [
+        'nom_utilisateur' => $username,
+        'greeting' => $greeting
+    ]);
+});
